0b547c3f4d9ec6eeeabc9c0b01dc267eada6b6e8
Max Miller <maxmiller413@gmail.com>
2021-01-28 17:29:19 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,16 +1,13 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
-  
-  array.count do |string|
-    string.class == String 
+  array.count do |strings|
+    strings.class == String 
   end 
-  
 end
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
-  
-  array.count do |string|
-    string == ""
+  array.count do |empty_strings|
+    empty_strings == ""
   end 
 end
